46aa2e526c0949758455990bcc3c228181bd9f41
dittyAvramovich <67556022+dittyAvramovich@users.noreply.github.com>
2023-05-04 11:06:25 +0300

    commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { ITreeOptions } from '@circlon/angular-tree-component';
+import { debounceTime, delay, distinctUntilChanged, map, mergeMap, of, Subject, Subscription } from 'rxjs';
+import { TreeService } from './tree/tree.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+
+  public nodes: any;
+  search: string = "";
+  public keyUp = new Subject<KeyboardEvent>();
+  private subscription!: Subscription;
+
+  constructor(private treeService: TreeService,
+    public cdRef: ChangeDetectorRef) {
+    this.subscription = this.keyUp.pipe(
+      map(event => this.search),
+      debounceTime(1000),
+      distinctUntilChanged(),
+      mergeMap(search => of(search).pipe(
+        delay(500),
+      )),
+    ).subscribe(p => {
+      this.treeService.filter(p).subscribe(res => {
+        this.nodes = res;
+        this.cdRef.detectChanges();
+      })
+    });
+  }
+
+  ngOnInit() {
+    this.treeService.fetchNodes().subscribe(res => {
+      this.nodes = res;
+    });
+  }
+}
